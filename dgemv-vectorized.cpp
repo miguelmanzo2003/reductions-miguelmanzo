ca8d555e7544286bff01fe51b9ea6bbef3ac6709
Miguel Manzo <miguelmanzo@MacBookAir.hsd1.ca.comcast.net>
2025-11-13 06:37:26 -0800

    added code for q3

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,5 +8,19 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
+    const double* __restrict a  = A;
+    const double* __restrict xv = x;
+    double* __restrict yv       = y;
 
+    for (int i = 0; i < n; ++i) {
+        double s = 0.0;
+        const double* __restrict Ai = a + (long)i * n;
+
+        // Inner dot-product reduction across the row: vectorizes
+        #pragma omp simd reduction(+:s)
+        for (int j = 0; j < n; ++j) {
+            s += Ai[j] * xv[j];
+        }
+        yv[i] += s;  // Y := A*X + Y
+    }
 }
